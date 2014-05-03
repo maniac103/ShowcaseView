d8d7982ae96204a25f43af0919a41d3abe141702
Danny Baumann <dannybaumann@web.de>
2014-05-03 13:55:15 +0200

    Make text positioning behaviour configurable.
    The current behaviour isn't well suited for e.g. longer text if the
    largest area happens to be to the right of the showcase.

--- library/src/main/java/com/github/amlcurran/showcaseview/ShowcaseView.java
+++ library/src/main/java/com/github/amlcurran/showcaseview/ShowcaseView.java
@@ -47,6 +47,12 @@ public class ShowcaseView extends RelativeLayout
 
     private static final int HOLO_BLUE = Color.parseColor("#33B5E5");
 
+    public enum TextPositioningMode {
+        AUTOMATIC,
+        ABOVE_OR_BELOW,
+        LEFT_OR_RIGHT
+    }
+
     private final Button mEndButton;
     private final TextDrawer textDrawer;
     private final ShowcaseDrawer showcaseDrawer;
@@ -67,6 +73,7 @@ public class ShowcaseView extends RelativeLayout
     private boolean hasAlteredText = false;
     private boolean hasNoTarget = false;
     private boolean shouldCentreText;
+    private TextPositioningMode textPositioning = TextPositioningMode.AUTOMATIC;
     private Bitmap bitmapBuffer;
 
     // Animation items
@@ -247,7 +254,8 @@ public class ShowcaseView extends RelativeLayout
         boolean recalculatedCling = showcaseAreaCalculator.calculateShowcaseRect(showcaseX, showcaseY, showcaseDrawer);
         boolean recalculateText = recalculatedCling || hasAlteredText;
         if (recalculateText) {
-            textDrawer.calculateTextPosition(getMeasuredWidth(), getMeasuredHeight(), this, shouldCentreText);
+            textDrawer.calculateTextPosition(getMeasuredWidth(), getMeasuredHeight(), this,
+                    textPositioning, shouldCentreText);
         }
         hasAlteredText = false;
         return true;
@@ -509,6 +517,11 @@ public class ShowcaseView extends RelativeLayout
             showcaseView.setOnShowcaseEventListener(showcaseEventListener);
             return this;
         }
+
+        public Builder setTextPositioning(TextPositioningMode textPositioning) {
+            showcaseView.setTextPositioning(textPositioning);
+            return this;
+        }
     }
 
     /**
@@ -516,6 +529,12 @@ public class ShowcaseView extends RelativeLayout
      */
     public void setShouldCentreText(boolean shouldCentreText) {
         this.shouldCentreText = shouldCentreText;
+        hasAlteredText = true;
+        invalidate();
+    }
+
+    public void setTextPositioning(TextPositioningMode textPositioning) {
+        this.textPositioning = textPositioning;
         hasAlteredText = true;
         invalidate();
     }
